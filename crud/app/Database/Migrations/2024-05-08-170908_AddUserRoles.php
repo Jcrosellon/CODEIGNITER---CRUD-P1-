7776d515cf519bfc98e5f23247773c73c3ef4443
Jcrosellon <119710206+Jcrosellon@users.noreply.github.com>
2024-05-08 13:14:22 -0500

    Nuevo cambio
    Se configuran los archivos de migración

--- crud/app/Database/Migrations/2024-05-08-170908_AddUserRoles.php
+++ crud/app/Database/Migrations/2024-05-08-170908_AddUserRoles.php
@@ -8,11 +8,39 @@ class AddUserRoles extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'Roles_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'Roles_name' => [
+                'type' => 'VARCHAR',
+                'unique' => true,
+                'constraint' => 30,
+            ],
+            'Roles_description' => [
+                'type' => 'VARCHAR',
+                'constraint' => 300,
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('Roles_id');
+        $this->forge->createTable('roles');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('roles');
     }
 }
--- crud/app/Database/Migrations/2024-05-08-170928_AddProfiles.php
+++ crud/app/Database/Migrations/2024-05-08-170928_AddProfiles.php
@@ -8,11 +8,50 @@ class AddProfiles extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'Profile_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'Profile_email' => [
+                'type' => 'VARCHAR',
+                'unique' => true,
+                'constraint' => 255,
+            ],
+            'Profile_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 30,
+            ],
+            'Profile_photo' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'User_id_fk' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('Profile_id');
+        $this->forge->addForeignKey('User_id_fk', 'users', 'User_id', 'CASCADE', 'SET NULL');
+        $this->forge->createTable('profiles');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('profiles');
     }
 }
